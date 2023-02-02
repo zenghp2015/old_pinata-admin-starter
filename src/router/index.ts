4c593bd93f949b48f8a5d3b1ffa634b3db7e6e01
zeng.hongpeng <zeng.hongpeng_wb2017@hellogroup.com>
2023-02-02 21:16:43 +0800

    feat:

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,27 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../pages/HomeView.vue";
+import {
+  type RouteRecordRaw,
+  createRouter,
+  createWebHistory,
+} from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "root",
+    redirect: "",
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../pages/AboutView.vue"),
-    },
-  ],
+  routes,
+  scrollBehavior() {
+    return {
+      el: "#app",
+      top: 0,
+      behavior: "smooth",
+    };
+  },
 });
 
 export default router;
