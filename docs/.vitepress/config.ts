27fb78f0549c64ce87bb49fef08b2b6467c7e480
zeng.hongpeng <zeng.hongpeng_wb2017@hellogroup.com>
2023-02-02 16:34:11 +0800

    feat: docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,8 +4,21 @@ export default defineConfig({
   lang: 'zh-CN',
   title: 'Pinata',
   description: '一个中后台的解决方案',
+
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '指南', link: '/guide' },
+      {
+        text: '生态系统',
+        items: [
+          { text: '主题', link: '/ecosystem/themes' },
+        ]
+      }
+    ]
+  },
+
   // vite 配置
-  
   vite: {
     server: {
       host: '0.0.0.0',
